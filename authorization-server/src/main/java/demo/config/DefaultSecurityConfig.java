169a63827b0eaf076d7b008a3c47ca1f22a1ad2b
kim.yuan <yuanjianfengkim@gmail.com>
2023-09-20 21:50:08 +0800

    authorization_code work should disable csrf

--- authorization-server/src/main/java/demo/config/DefaultSecurityConfig.java
+++ authorization-server/src/main/java/demo/config/DefaultSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,7 +27,9 @@ public class DefaultSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeRequests ->
+        http.csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests.anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults());
